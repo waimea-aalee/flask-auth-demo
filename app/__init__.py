de142b7ba02589136ca0e6c63f51f65594a135da
Azaria <168692066+waimea-aalee@users.noreply.github.com>
2025-06-10 09:59:15 +1200

    New changes

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,12 @@
 # App Creation and Launch
 #===========================================================
 
-from flask import Flask, render_template, request, flash, redirect
+from flask import Flask, render_template, session, request, flash, redirect
 from werkzeug.security import generate_password_hash, check_password_hash
 import html
 
 from app.helpers.session import init_session
-from app.helpers.db import connect_db, handle_db_errors
+from app.helpers.db import connect_db
 from app.helpers.errors import register_error_handlers, not_found_error
 
 
@@ -36,6 +36,7 @@ def index():
 def about():
     return render_template("pages/about.jinja")
 
+
 #-----------------------------------------------------------
 # Sign Up page route
 #-----------------------------------------------------------
@@ -45,10 +46,17 @@ def signup():
 
 
 #-----------------------------------------------------------
+# Login page route
+#-----------------------------------------------------------
+@app.get("/login/")
+def login():
+    return render_template("pages/login.jinja")
+
+
+#-----------------------------------------------------------
 # Things page route - Show all the things, and new thing form
 #-----------------------------------------------------------
 @app.get("/things/")
-@handle_db_errors
 def show_all_things():
     with connect_db() as client:
         # Get all the things from the DB
@@ -58,13 +66,12 @@ def show_all_things():
 
         # And show them on the page
         return render_template("pages/things.jinja", things=things)
-
+    
 
 #-----------------------------------------------------------
 # Thing page route - Show details of a single thing
 #-----------------------------------------------------------
 @app.get("/thing/<int:id>")
-@handle_db_errors
 def show_one_thing(id):
     with connect_db() as client:
         # Get the thing details from the DB
@@ -96,7 +103,6 @@ def show_one_thing(id):
 # Route for adding a thing, using data posted from a form
 #-----------------------------------------------------------
 @app.post("/add")
-@handle_db_errors
 def add_a_thing():
     # Get the data from the form
     name  = request.form.get("name")
@@ -116,11 +122,11 @@ def add_a_thing():
         flash(f"Thing '{name}' added", "success")
         return redirect("/things")
 
+
 #-----------------------------------------------------------
 # Route for adding a user, using data posted from a form
 #-----------------------------------------------------------
 @app.post("/add-user")
-@handle_db_errors
 def add_a_user():
     # Get the data from the form
     name      = request.form.get("name")
@@ -145,12 +151,48 @@ def add_a_user():
         return redirect("/")
 
 
+#-----------------------------------------------------------
+# Route for logging in a user, using data posted from a form
+#-----------------------------------------------------------
+@app.post("/login-user")
+def login_user():
+    # Get the data from the form
+    username  = request.form.get("username")
+    password  = request.form.get("password")
+
+    # Sanitise the inputs
+    username = html.escape(username)
+
+
+    with connect_db() as client:
+        # Try to find a matching record    
+        sql = "SELECT name, password_hash FROM users WHERE username=?)"
+        values = [username]
+        result = client.execute(sql, values)
+
+        # Check if we got a record
+        if result.rows:
+            # Yes, so user exists
+            user = result.rows[0]
+            hash = user["password_hash"]
+
+            # Check if passwords match
+            if check_password_hash(hash, password):
+                # Yes, so save the details in the session
+                session["user_id"] = user["id"]
+                session["user_name"] = user["name"]
+                flash("Logged in successfully", "success")
+                return redirect("/")
+
+        # Go back to the home page
+        flash("Incorrect credentials", "error")
+        return redirect("/login")
+
 
 #-----------------------------------------------------------
 # Route for deleting a thing, Id given in the route
 #-----------------------------------------------------------
 @app.get("/delete/<int:id>")
-@handle_db_errors
 def delete_a_thing(id):
     with connect_db() as client:
         # Delete the thing from the DB
